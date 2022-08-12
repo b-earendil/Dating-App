a521f48c6ff47a320af2051260227ae0cc432c06
Ben Adams <BKAdams@jhacorp.com>
2022-08-12 16:57:52 -0500

    Minor updates

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'The Dating App';
-  users: any;
+  users: any; // remove type safety
 
   constructor(private http: HttpClient) {}
 
@@ -17,19 +17,8 @@ export class AppComponent implements OnInit {
   }
 
   // this subscribe() method has been deprecated and may produce warnings
-  // get weatherforecast basketball players from dapper-demo test-service
-  getUsers() {
-    this.http.get('http://localhost:6000/weather').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
-  }
-
-  // this subscribe() method has been deprecated and may produce warnings
-  // get api/users, dating app project
-  //     getUsers() {
-  //     this.http.get('https://localhost:5003/api/users').subscribe(response => {
+  // getUsers() {
+  //   this.http.get('https://localhost:5001/api/users').subscribe(response => {
   //     this.users = response;
   //   }, error => {
   //     console.log(error);
@@ -37,10 +26,10 @@ export class AppComponent implements OnInit {
   // }
 
   // non-deprecated observer pattern for subscribe method
-  // getUsers() {
-  //   this.http.get('https://localhost:5003/api/users').subscribe({
-  //     next: response => this.users = response,
-  //     error: error => console.log(error)
-  //   })
-  // }
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error)
+    })
+  }
 }
